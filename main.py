d81b9f9e6bf96c79a948657ee24c67c5ad894b53
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-03 11:36:26 +0300

    1

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-import os
-
 import telebot
+import os
 from flask import Flask, request
 
-TOKEN = '610980315:AAE494y1vZOwGeNmisevy-3OtcMwJD_JpVs'
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+bot = telebot.TeleBot('610980315:AAE494y1vZOwGeNmisevy-3OtcMwJD_JpVs')
 
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
 
-
-@server.route('/' + TOKEN, methods=['POST'])
+@server.route("/bot", methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://richeco.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url="https://richeco.herokuapp.com/")
     return "!", 200
 
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+server = Flask(__name__)
