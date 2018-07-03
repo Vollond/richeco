a84f4cb2b12dd7f2d6eefbcb9545005d0f845b3e
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-03 20:24:42 +0300

    1

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def callback_inline(call):
 			cursor2.execute(f"UPDATE users SET coin = coin + 1 WHERE user_id={userid}")
 			cursor2.execute(f"select coin from users where user_id={userid}")
 			results2 = cursor2.fetchall()
-			''.join(results2)
+			results2=str.join(results2)
 			conn.commit()
 			conn.close()
 			print (results2)
