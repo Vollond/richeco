459f8bb15447bd527ebc0c9a6815ec28de9b284e
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-03 22:40:17 +0300

    1

--- main.py
+++ main.py
@@ -64,7 +64,6 @@ def callback_inline(call):
 			cursor2.execute(f"select coin from users where user_id={userid}")
 			results2 = cursor2.fetchall()
 			''.join(str(e) for e in results2)
-			print (b) 
 			results = b
 			conn.commit()
 			conn.close()
