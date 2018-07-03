f38fce7748bff50fa1a5b8dd583ae2897358bb3d
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-03 17:19:46 +0300

    1

--- main.py
+++ main.py
@@ -43,6 +43,14 @@ def start(message):
 	conn.close()
 	bot.reply_to(message, 'pong, ' + message.from_user.first_name)
 
+@bot.message_handler(commands=['work2'])
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    work_button = types.InlineKeyboardButton(text="Перейти на Яндекс")
+    keyboard.add(work_button)
+    bot.send_message(message.chat.id, "Работать", reply_markup=keyboard)	
+	
+	
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
