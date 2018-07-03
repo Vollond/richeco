06181401f6fc7bb426d4dc8aedd76dccb85e1712
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-03 12:13:27 +0300

    1

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, 'Helo, ' + message.from_user.first_name)
 
 @bot.message_handler(commands=['ping'])
 def start(message):
     bot.reply_to(message, 'pong, ' + message.from_user.first_name)	
+	
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
 
 	
 
