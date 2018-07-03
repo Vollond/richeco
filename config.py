01dd69d9725308e098eed6851c7c63311ae87411
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-03 10:32:30 +0300

    1

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# webhook
+URL = os.environ.get('URL')
+PORT = int(os.environ.get('PORT', '5000'))
+
+# telegram
+TOKEN = os.environ.get('610980315:AAE494y1vZOwGeNmisevy-3OtcMwJD_JpVs')
