4eeb14cce69d787f981c41986c72b666b3420c31
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-04 01:38:36 +0300

    1

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 _default_data = lambda: defaultdict(_default_data)
 data = _default_data()
-
+import json
 data["server"]["host"] = "127.0.0.1"
 data["server"]["port"] = "22"
 
@@ -18,9 +18,12 @@ stats["exempls"]["head"] = "w"
 stats["exempls"]["legs"] = "l"
 stats["exempls"]["loolz"] = "123"
 
+stats = json.dumps(stats)
+stats = json.loads(stats)
+
+print (stats["health"]["hp"])
 
 
-import json
 print (json.dumps(data, indent=2))
 print (json.dumps(stats, indent=5))
 print (stats["exempls"]["head"])
