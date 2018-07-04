4882f2a255a66b1561cc52fe53ea338c3c16223f
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-04 15:49:51 +0300

    1

--- funs.py
+++ funs.py
@@ -13,7 +13,7 @@ DATABASE_URL = os.environ['DATABASE_URL']
 bot = telebot.TeleBot('610980315:AAE494y1vZOwGeNmisevy-3OtcMwJD_JpVs')
 server = Flask(__name__)
 
-def f_coin (op,userid)
+def f_coin (op,userid):
 	if op = '?':
 		conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 		cursor = conn.cursor()
