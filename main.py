fc4023bd4fd7ef91967144747ff52c83e06c9001
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-04 15:06:50 +0300

    1

--- main.py
+++ main.py
@@ -43,11 +43,11 @@ def start(message):
 @bot.message_handler(commands=['work'])
 def default_test(message):
     keyboard = types.InlineKeyboardMarkup()
-    work_button = types.InlineKeyboardButton(text="Кликай, чтобы заработать!", callback_data="work")
+	work_button = types.InlineKeyboardButton(text="Кликай, чтобы заработать!", callback_data="work")
 	work_button = types.InlineKeyboardButton(text="Отправить рабочих в експедицию", callback_data="exped_button")
-    keyboard.add(work_button)
+	keyboard.add(work_button)
 	keyboard.add(exped_button)
-    bot.send_message(message.chat.id, "Работать", reply_markup=keyboard)	
+	bot.send_message(message.chat.id, "Работать", reply_markup=keyboard)	
 	
 @bot.message_handler(commands=['build'])
 def default_test(message):
