3f477118883f4d927a311ba4c24db9dcf3200e36
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-06 01:54:26 +0300

    1

--- main.py
+++ main.py
@@ -23,13 +23,17 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['wr1'])
 def start(message):
-	bot.send_message(chat_id=message.chat.id, text="""
+	bot.send_message(chat_id=message.chat.id, text='123', parse_mode='Markdown')
+'''
+	"""
 	Рабочие нашли: 
 	'Оно [ça] * функционирует повсюду, иногда без остановок, иногда с перерывами. 
 	Оно дышит, оно греет, оно ест. 
 	Оно испражняется, оно целует. 
 	Но какое заблуждение говорить о нем как о чем-то одном и определенном [le ça].' 
-	""", parse_mode='Markdown')
+	"""
+'''
+	
 					
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -37,7 +41,7 @@ def start(message):
 	
 @bot.message_handler(commands=['ping'])
 def start(message):
-    bot.send_message(chat_id=message.chat.id, text="'123' 123**123** 11''231231''2312123", parse_mode='Markdown')
+    bot.send_message(chat_id=message.chat.id, text="'123' `inline fixed-width code`11''2312*bold text*31''2312123", parse_mode='Markdown')
 	
 @bot.message_handler(commands=['help'])
 def start(message):
