da4aa47ba5eb07a8d30136186407662080f3c199
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-06 01:14:01 +0300

    1

--- main.py
+++ main.py
@@ -291,7 +291,7 @@ def callback_inline(call):
 			else:
 				bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="У вас уже отправленна експедиция!")				
 
-#######################
+#########################
 @bot.message_handler(commands=['calendar'])
 def get_calendar(message):
     now = datetime.datetime.now() #Current date
