dd4f9f91b400048fcb3358db9b22424a1f0aeb1a
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-06 02:25:21 +0300

    1

--- main.py
+++ main.py
@@ -29,19 +29,21 @@ def start(message):
 	"""), parse_mode='Markdown')
 	msgid=message.message_id+1
 	time.sleep(1) 	
-	bot.edit_message_text(chat_id=message.chat.id, (f"""
+	bot.edit_message_text(chat_id=message.chat.id, msgid, (f"""
 Рабочие нашли помятый кусок свитка: 
 `Оно [ça] * функционирует повсюду, иногда без остановок, иногда с перерывами. 
 Оно дышит, оно греет, оно ест.` 
 	"""), parse_mode='Markdown')
 	time.sleep(1) 	
-	bot.edit_message_text(chat_id=message.chat.id, (f"""
+	bot.edit_message_text(chat_id=message.chat.id, msgid, (f"""
 Рабочие нашли помятый кусок свитка: 
 `Оно [ça] * функционирует повсюду, иногда без остановок, иногда с перерывами. 
 Оно дышит, оно греет, оно ест. 
 Оно испражняется, оно целует.` 
 	"""), parse_mode='Markdown')
-	time.sleep(1) 					
+	time.sleep(1) 		
+
+	
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Helo, ' + message.from_user.first_name)
