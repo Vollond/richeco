a89ca23ac3703d21410442888704d9268a53ee4c
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-06 02:22:47 +0300

    1

--- main.py
+++ main.py
@@ -25,12 +25,23 @@ server = Flask(__name__)
 def start(message):
 	bot.send_message(message.chat.id, (f"""
 Рабочие нашли помятый кусок свитка: 
+`Оно [ça] * функционирует повсюду, иногда без остановок, иногда с перерывами.` 
+	"""), parse_mode='Markdown')
+	msgid=message.message_id+1
+	time.sleep(1) 	
+	bot.edit_message_text(chat_id=message.chat.id, (f"""
+Рабочие нашли помятый кусок свитка: 
+`Оно [ça] * функционирует повсюду, иногда без остановок, иногда с перерывами. 
+Оно дышит, оно греет, оно ест.` 
+	"""), parse_mode='Markdown')
+	time.sleep(1) 	
+	bot.edit_message_text(chat_id=message.chat.id, (f"""
+Рабочие нашли помятый кусок свитка: 
 `Оно [ça] * функционирует повсюду, иногда без остановок, иногда с перерывами. 
 Оно дышит, оно греет, оно ест. 
-Оно испражняется, оно целует. 
-Но какое заблуждение говорить о нем как о чем-то одном и определенном [le ça].` 
+Оно испражняется, оно целует.` 
 	"""), parse_mode='Markdown')
-					
+	time.sleep(1) 					
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Helo, ' + message.from_user.first_name)
