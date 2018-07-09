a7284630649ea4aba3b7388029da956f50f31283
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-09 16:49:39 +0300

    1

--- task.py
+++ task.py
@@ -1,2 +1,18 @@
+import telebot
+import os
+from flask import Flask, request
+import psycopg2
+from telebot import types
+import re
+import json
+from collections import defaultdict
+_default_data = lambda: defaultdict(_default_data)
+import time
+from funs import f_coin
+from funs import f_builds
+import random
+from battles import defens
+from telegramcalendar import create_calendar
+import datetime
 from main import task_test
 task_test()
