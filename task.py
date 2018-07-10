3670ec5dc6af258d56b80f141b93f9847bfc37e7
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-11 02:09:09 +0300

    1

--- task.py
+++ task.py
@@ -20,7 +20,29 @@ import datetime
 DATABASE_URL = os.environ['DATABASE_URL']
 bot = telebot.TeleBot('610980315:AAE494y1vZOwGeNmisevy-3OtcMwJD_JpVs')
 
-
+i=0
+while (i<9):
+	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+	cursor = conn.cursor()
+	now=time.time()
+	cursor.execute(f"select action, user_id, id from tasks where time < {now}")
+	act = cursor.fetchall()
+	if(act!=[]):
+		print (act)
+		action = act[0][0]
+		user_id = act[0][1]
+		id = act[0][2]
+		print(action)
+		print(user_id)
+		print(id)
+		cursor.execute(f"delete from tasks where id={id}")
+		bot.send_message(user_id, (f"={id}"))	
+	conn.commit()
+	conn.close()
+	time.sleep(70) 	
+	i+=1 	
+	
+	
 def my_tasks_cron ():
 	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 	cursor = conn.cursor()
@@ -39,16 +61,7 @@ def my_tasks_cron ():
 		bot.send_message(user_id, (f"={id}"))	
 	conn.commit()
 	conn.close()
-	 	
 
-
-def loop ():
-	i=0
-	while (i<9):
-		my_tasks_cron()
-		time.sleep(70) 	
-		i+=1
-loop()	
 '''
 print ('1')
 import main
