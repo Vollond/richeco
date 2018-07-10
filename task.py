79d2c633c75d4a22145f0c39f836f5855a2e9634
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-10 03:48:59 +0300

    1

--- task.py
+++ task.py
@@ -21,17 +21,17 @@ DATABASE_URL = os.environ['DATABASE_URL']
 bot = telebot.TeleBot('610980315:AAE494y1vZOwGeNmisevy-3OtcMwJD_JpVs')
 bot.send_message(322682583, "таск")	
 
-def task_loop():
-	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-	cursor = conn.cursor()
-	now=time.time()
-	cursor.execute(f"select action from tasks where time < {now}")
-	act = cursor.fetchall()
-	print (act)
-	conn.commit()
-	conn.close()
 
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
+now=time.time()
+cursor.execute(f"select action from tasks where time < {now}")
+act = cursor.fetchall()
+print (act)
+conn.commit()
+conn.close()
 
+'''
 print ('1')
 import main
 print ('2')
@@ -39,3 +39,4 @@ from main import task_test
 task_test()
 print ('123')
 task_test()
+'''
