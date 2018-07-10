91e7200a1d889f08af732ceedd837670f13690d2
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-10 04:14:33 +0300

    1

--- task.py
+++ task.py
@@ -29,9 +29,9 @@ while (i<9):
 	cursor.execute(f"select action, user_id, id from tasks where time < {now}")
 	act = cursor.fetchall()
 	print (act)
-	action = act[0]
-	user_id = act[1]
-	id = act[2]
+	action = act[0][0]
+	user_id = act[0][1]
+	id = act[0][2]
 	#cursor.execute(f"delete from tasks where id={id}")
 	bot.send_message(322682583, (f"={action}"))	
 	conn.commit()
