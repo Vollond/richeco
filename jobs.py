288124fc20c401b54f48e1e0f65f36c9e1c15072
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-11 12:51:53 +0300

    1

--- jobs.py
+++ jobs.py
@@ -3,12 +3,12 @@ from task2 import my_tasks_cron
 from hour_tasks import hour_tasks_cron
 
 sched = BlockingScheduler()
-
+'''
 @sched.scheduled_job('interval', minutes=1)
 def print_data():
 	print("Have a good day!")
-
-@sched.scheduled_job('interval', minutes=10)
+'''
+@sched.scheduled_job('interval', minutes=1)
 def update_a():
  	my_tasks_cron()
 
